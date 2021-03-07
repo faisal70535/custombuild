02f7e266bd928b68d01693aea744d7783a41ec33
unknown <faisal70535@gmail.com>
2021-03-07 12:14:43 +0530

    adding c integer value file

---
+++ c-beginner/input-integer-value.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+    printf("Enter the A and B value :-");
+    scanf("%d%d", &x, &y);
+    z=x+y;
+    printf("Sum x +y is :-%d", z);
+
+    return 0;
+}
